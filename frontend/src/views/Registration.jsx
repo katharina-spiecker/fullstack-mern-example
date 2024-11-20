d4352586aee5bbe9dd397e0bb7e644b74867a73a
katharina-spiecker <kathispiecker@gmail.com>
2024-11-20 22:37:28 +0100

    fix

--- frontend/src/views/Registration.jsx
+++ frontend/src/views/Registration.jsx
@@ -9,7 +9,7 @@ export default function Registration() {
     function onSubmit(e) {
         e.preventDefault();
 
-        fetch('https://fullstack-mern-example.onrender.com/auth/api/register', {
+        fetch('https://fullstack-mern-example.onrender.com/api/auth/register', {
             method: 'POST', // HTTP-Methode
             headers: {
               'Content-Type': 'application/json' // Header, der den Inhaltstyp angibt
